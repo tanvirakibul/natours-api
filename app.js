f07ea5118c89fe12602e02e436c6e346e08de4da
MD. Rakibul Hassan <tanvirakibul@gmail.com>
2020-10-23 21:55:55 +0600

    started the building of api

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+//
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
