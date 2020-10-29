ccc6642ef76e1fa067aa66ea4285296da7ea8ea9
MD. Rakibul Hassan <tanvirakibul@gmail.com>
2020-10-29 22:54:37 +0600

    added our middleware

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("Hello from the middleware! 👋");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTour = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     result: tours.length,
     data: {
       tours
